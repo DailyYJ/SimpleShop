4e92e036f0cb5ae7b488f0183b1e1ce15d6f2a3f
DailyYJ <jeonyj4758@gmail.com>
2020-12-20 23:49:11 +0900

    JPA Simple Shop First Commit

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+//    @Transactional
+//    void update(Item itemParam) { //itemParam: 파리미터로 넘어온 준영속 상태의 엔티티
+//        Item findItem = em.find(Item.class, itemParam.getId()); //같은 엔티티를 조회한다.
+//                findItem.setPrice(itemParam.getPrice()); //데이터를 수정한다.
+//    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,47 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true) // 읽기 전용으로하면 성능상 더 효율 좋음 아래에는 읽기전용이 많으니까 기본값으로 두고 read에는 따로적용
+@RequiredArgsConstructor // final 필드만 생성자 만들어줌
+public class MemberService {
+
+    @Autowired
+    private final MemberRepository memberRepository;
+
+    /*
+    회원가입
+    Transactional
+    */
+    @Transactional
+    public Long join(Member member){
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        //EXCEPTION
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId){
+        return memberRepository.findOne(memberId);
+    }
+}
